62ebe61cd571eedd844ade6aeec114fcbde30e79
Eagle-Min <88953692+Eagle-Min@users.noreply.github.com>
2022-03-17 11:19:53 +0700

    commit bouncing-ball

---
+++ bouncing_ball/assets/js/main.js
@@ -0,0 +1,77 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+        
+var x = Math.floor(Math.random() * ((canvas.width - 100) - 50)) + 50;
+var y = Math.floor(Math.random() * ((canvas.height - 100) - 50)) + 50;
+
+var dx = (Math.random() * (1 - (-1))) + (-1);
+var dy = (Math.random() * (1 - (-1))) + (-1);
+
+var ballRadius = 20;
+        
+var upPressed = false;
+var downPressed = false;
+
+document.addEventListener("keydown", keyDownHandler);
+document.addEventListener("keyup", keyUpHandler);
+
+function keyDownHandler(e) {
+    if(e.key == "Up" || e.key == "ArrowUp") {
+        upPressed = true;
+    }
+    else if(e.key == "Down" || e.key == "ArrowDown") {
+        downPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "Up" || e.key == "ArrowUp") {
+        upPressed = false;
+    }
+    else if(e.key == "Down" || e.key == "ArrowDown") {
+        downPressed = false; 
+    }
+}
+
+function drawBall() {
+    ctx.beginPath();
+    // ctx.fillRect(x, y, 100, 100);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+
+    if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+
+    if(y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+
+    if(upPressed) {
+        dx *= 1.02;
+        dy *= 1.02;
+    }
+    else if(downPressed) {
+        dx /= 1.02;
+        dy /= 1.02;
+    }
+
+    x += dx;
+    y += dy;
+}
+
+    window.addEventListener('resize', () => {
+        canvas.width = window.innerWidth
+        canvas.height = window.innerHeight
+        draw(canvas)
+    })
+    setInterval(draw, 10);
