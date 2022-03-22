7e86f9565a1379f4af424ee42f4743199ad0f435
Eagle-Min <88953692+Eagle-Min@users.noreply.github.com>
2022-03-22 13:01:18 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-// Giá trị mặc định.
+// Default value
 const $ = document.querySelector.bind(document);
 const $width = window.innerWidth;
 const $height = window.innerHeight;
@@ -8,24 +8,30 @@ ball.width = $width;
 ball.height = $height;
 const ctx = ball.getContext("2d");
 
-// Resize windowns
-window.addEventListener("resize", (e) => {
+// Resize window
+window.addEventListener("resize", () => {
   ball.width = window.innerWidth;
   ball.height = window.innerHeight;
+  oneBall = new Ball(Math.floor(Math.random() * ((ball.width - 100) - 50)) + 50, 
+                      Math.floor(Math.random() * ((ball.height - 100) - 50)) + 50,
+                      15, 
+                      (Math.random() * (1 - (-1))) + (-1),
+                      (Math.random() * (1 - (-1))) + (-1),
+                      'white');
 });
 
 // Class Ball
 class Ball {
   constructor(x, y, radius, sdx, sdy, color) {
-    this.x = x;                     // Tọa độ ban đầu theo trục x của ball.
-    this.y = y;                     // Tọa độ ban đầu theo trục y của ball.
-    this.radius = radius;           // Bán kính của ball.
-    this.sdx = sdx;                 // Tốc độ ban đầu theo trục x của ball.
-    this.sdy = sdy;                 // Tốc độ ban đầu theo trục y của ball.
-    this.color = color;             // Màu của bóng.
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.sdx = sdx;
+    this.sdy = sdy;
+    this.color = color;
   }
 
-  // Vẽ ball
+  // Draw ball
   draw() {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
@@ -35,18 +41,18 @@ class Ball {
     this.move();
   }
 
-  // Di chuyển ball
+  // Move ball
   move() {
     this.x += this.sdx;
     this.y += this.sdy;
   }
 
-  // Check va chạm của ball với 4 hướng
+  // Check
   check() {
     if (this.x + this.radius > ball.width) {
       this.sdx = -this.sdx;
     }
-    if (this.y + this.radius > ball.height) {
+    if (this.y + this.radius > ball.height) { 
       this.sdy = -this.sdy;
     }
     if (this.x - this.radius < 0) {
@@ -57,45 +63,151 @@ class Ball {
     }
   }
 
-  // Tăng tốc ball
+  // Speed up
   speedUp() {
     this.sdx *= 1.1;
     this.sdy *= 1.1;
   }
 
-  // Giảm tốc ball
+  // Speed down
   speedDown() {
     this.sdx /= 1.1;
     this.sdy /= 1.1;
   }
 }
-   
-// Truyền giá trị cho ball
-const oneBall = new Ball(Math.floor(Math.random() * ((ball.width - 100) - 50)) + 50,
-                         Math.floor(Math.random() * ((ball.height - 100) - 50)) + 50,
-                         15,
-                         (Math.random() * (1 - (-1))) + (-1),
-                         (Math.random() * (1 - (-1))) + (-1),
-                         'white');
+       
+// The value of ball
+var oneBall = new Ball( Math.floor(Math.random() * ((ball.width - 100) - 50)) + 50,
+                        Math.floor(Math.random() * ((ball.height - 100) - 50)) + 50,
+                        15,
+                        (Math.random() * (1 - (-1))) + (-1),
+                        (Math.random() * (1 - (-1))) + (-1),
+                        'white');
 
-// Chuyển động ball
+// Ball motion
 const ballAnimation = () => {
   ctx.clearRect(0, 0, ball.width, ball.height);
   oneBall.draw();
   oneBall.check();
-};
-setInterval(ballAnimation, 30);
 
-// Speed with keybroad
+};
+setInterval(ballAnimation, 20);
+
+// Speed with keyboard
 document.addEventListener(
   "keydown",
   (event) => {
     if (event.code === "ArrowUp") {
-        oneBall.speedUp();
+      oneBall.speedUp();
     }
     if (event.code === "ArrowDown") {
-        oneBall.speedDown();
+      oneBall.speedDown();
     }
   },
   false
 );
+
+
+// // Giá trị mặc định.
+// const $ = document.querySelector.bind(document);
+// const $width = window.innerWidth;
+// const $height = window.innerHeight;
+
+// const ball = $(".canvas");
+// ball.width = $width;
+// ball.height = $height;
+// const ctx = ball.getContext("2d");
+
+// // Resize windowns
+// window.addEventListener("resize", (e) => {
+//   ball.width = window.innerWidth;
+//   ball.height = window.innerHeight;
+//   // Ball.constructor(x, y, radius, sdx, sdy, color);
+//   oneBall = new Ball(50, 50, 15, 1, 1, 'red');
+// });
+
+// // Class Ball
+// class Ball {
+//   constructor(x, y, radius, sdx, sdy, color) {
+//     this.x = x;                     // Tọa độ ban đầu theo trục x của ball.
+//     this.y = y;                     // Tọa độ ban đầu theo trục y của ball.
+//     this.radius = radius;           // Bán kính của ball.
+//     this.sdx = sdx;                 // Tốc độ ban đầu theo trục x của ball.
+//     this.sdy = sdy;                 // Tốc độ ban đầu theo trục y của ball.
+//     this.color = color;             // Màu của bóng.
+//   }
+
+//   // Vẽ ball
+//   draw() {
+//     ctx.beginPath();
+//     ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+//     ctx.fillStyle = this.color;
+//     ctx.closePath();
+//     ctx.fill();
+//     this.move();
+//   }
+
+//   // Di chuyển ball
+//   move() {
+//     this.x += this.sdx;
+//     this.y += this.sdy;
+//   }
+
+//   // Check va chạm của ball với 4 hướng
+//   check() {
+//     if (this.x + this.radius > ball.width) {
+//       this.sdx = -this.sdx;
+//     }
+//     if (this.y + this.radius > ball.height) {
+//       this.sdy = -this.sdy;
+//     }
+//     if (this.x - this.radius < 0) {
+//       this.sdx = -this.sdx;
+//     }
+//     if (this.y - this.radius < 0) {
+//       this.sdy = -this.sdy;
+//     }
+//   }
+
+//   // Tăng tốc ball
+//   speedUp() {
+//     this.sdx *= 1.1;
+//     this.sdy *= 1.1;
+//   }
+
+//   // Giảm tốc ball
+//   speedDown() {
+//     this.sdx /= 1.1;
+//     this.sdy /= 1.1;
+//   }
+// }
+   
+// // Truyền giá trị cho ball
+// const oneBall = new Ball(Math.floor(Math.random() * ((ball.width - 100) - 50)) + 50,
+//                          Math.floor(Math.random() * ((ball.height - 100) - 50)) + 50,
+//                          15,
+//                          (Math.random() * (1 - (-1))) + (-1),
+//                          (Math.random() * (1 - (-1))) + (-1),
+//                          'white');
+
+// // Chuyển động ball
+// const ballAnimation = () => {
+//   ctx.clearRect(0, 0, ball.width, ball.height);
+//   oneBall.draw();
+//   oneBall.check();
+// };
+// setInterval(ballAnimation, 30);
+
+// // Speed with keybroad
+// document.addEventListener(
+//   "keydown",
+//   (event) => {
+//     if (event.code === "ArrowUp") {
+//         oneBall.speedUp();
+//     }
+//     if (event.code === "ArrowDown") {
+//         oneBall.speedDown();
+//     }
+//   },
+//   false
+// );
